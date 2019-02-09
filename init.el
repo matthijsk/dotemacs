a7d4797470e8b2a4c6cbdc54693db5ba7af13226
Matthijs Kool <matthijzk@gmail.com>
2019-02-09 16:53:44 +0100

    Pin most packages to melpa-stable, require use-package in init.el

--- init.el
+++ init.el
@@ -13,4 +13,9 @@
         ("gnu" . 5)
         ("melpa" . 0)))
 
+;; Use-package allows for automatic installation of packages, which
+;; is great when you want to use Emacs on multiple systems. Also, it
+;; loads packages when needed, keeping the Emacs startup time short.
+(require 'use-package)
+
 (org-babel-load-file "~/.emacs.d/configuration.org")

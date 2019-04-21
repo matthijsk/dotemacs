97b005a34e8b11977c54342063bd042ba5aafcaf
Matthijs Kool <matthijzk@gmail.com>
2019-04-21 21:57:20 +0200

    Only add melpa packages when using Emacs 25 and above
    The package-archive-priorities is available since 25, so for older
    Emacsen just stick to melpa-stable.

--- init.el
+++ init.el
@@ -1,17 +1,24 @@
 (require 'package)
 
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives'("melpa-stable" . "http://stable.melpa.org/packages/"))
 
+;; For important compatibility libraries like cl-lib
 (when (< emacs-major-version 24)
-  ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-(package-initialize)
 
-(setq package-archive-priorities
-      '(("melpa-stable" . 10)
-        ("gnu" . 5)
-        ("melpa" . 0)))
+;; Only add non-stable melpa when emacs major version is larger than 24. Emacs
+;; 25.1 introduced the package-archive-priorities variable, so for older
+;; versions just stick with the stable archive to prevent installing unwanted
+;; (unstable) packages.
+(when (> emacs-major-version 24)
+  (add-to-list 'package-archives'("melpa" . "http://melpa.org/packages/"))
+  (setq package-archive-priorities
+        '(("melpa-stable" . 10)
+          ("gnu" . 5)
+          ("melpa" . 0))))
+
+
+(package-initialize)
 
 ;; Bootstrap use-package. Use-package allows for automatic installation of
 ;; packages, which is great when you want to use Emacs on multiple systems.

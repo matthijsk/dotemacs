ae91a4ad48810fc095d03ea80057bf2dd8b02fae
Matthijs Kool <matthijzk@gmail.com>
2019-07-17 17:11:51 +0200

    Use https for package archive urls

--- init.el
+++ init.el
@@ -1,22 +1,21 @@
 (require 'package)
 
-(add-to-list 'package-archives'("melpa-stable" . "http://stable.melpa.org/packages/"))
+(add-to-list 'package-archives'("melpa-stable" . "https://stable.melpa.org/packages/"))
 
 ;; For important compatibility libraries like cl-lib
 (when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+  (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 
 ;; Only add non-stable melpa when emacs major version is larger than 24. Emacs
 ;; 25.1 introduced the package-archive-priorities variable, so for older
 ;; versions just stick with the stable archive to prevent installing unwanted
 ;; (unstable) packages.
 (when (> emacs-major-version 24)
-  (add-to-list 'package-archives'("melpa" . "http://melpa.org/packages/"))
+  (add-to-list 'package-archives'("melpa" . "https://melpa.org/packages/"))
   (setq package-archive-priorities
         '(("melpa-stable" . 10)
           ("gnu" . 5)
           ("melpa" . 0))))
-
 
 (package-initialize)
 

c69e090ec3dddd0c209c881f6360b8d08c278eff
Matthijs Kool <matthijzk@gmail.com>
2020-03-14 12:20:36 +0100

    Only require use-package when byte-compiling

--- init.el
+++ init.el
@@ -35,7 +35,9 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-(require 'use-package)
+(eval-when-compile
+  (require 'use-package))
+
 (setq use-package-always-ensure t)
 
 (defcustom my-configuration-file (concat user-emacs-directory "configuration.org")

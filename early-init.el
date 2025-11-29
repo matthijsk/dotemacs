6c0a510a2038f5b7b2f55977e9ea91c0fdf1b675
Matthijs Kool <matthijzk@gmail.com>
2025-11-29 23:03:00 +0100

    Remove early-init startup hack
    Performance gain is marginal on the systems I currently use. I prefer using
    defaults.

--- early-init.el
+++ early-init.el
@@ -1,14 +1,1 @@
-;; Optimize startup by setting a very large gc cons threshold value. Restore the
-;; value after start up is done to prevent noticeable gc sweeps.
-;;
-;; https://www.reddit.com/r/emacs/comments/ofhket/further_boost_start_up_time_with_a_simple_tweak/h4fxqyn?utm_source=share&utm_medium=web2x&context=3
-(defvar old-file-name-handler file-name-handler-alist)
-(setq file-name-handler-alist nil
-      gc-cons-threshold most-positive-fixnum)
-
-(add-hook 'after-init-hook (lambda nil
-                             (setq gc-cons-threshold     1600000
-                                   gc-cons-percentage   0.1
-                                   file-name-handler-alist old-file-name-handler)))
-
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
